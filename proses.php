dfea8c3fc5da4f742c3e15071b467efef40b3fb9
Irvin Naufal <42725269+irvinnaufal@users.noreply.github.com>
2018-09-10 09:55:56 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,10 @@
 <?php 
-//Tuliskan kode anda di bawah ini
+$jumlah = $_GET['jumlah'];
+for ($i=0 ; $i <$jumlah ; $i++) { 
+	for ($j=$i; $j<$jumlah; $j++) { 
+		echo "*";
+	}
+	echo "<br>";
+}
 
 ?>
